9e3e70400da74338cd375a4df69725b9fbbb855a
ROJS <mskim829@naver.com>
2025-12-01 22:57:21 +0900

    Add OutlineShader

--- main.gd
+++ main.gd
@@ -1,4 +1,12 @@
 extends Node2D
 
+signal turn_started(turn : int)
+signal turn_ended(turn : int)
+
 var current_turn : int = 0
 var units : Array[Unit] = []
+
+func switch_turn()->void:
+	turn_ended.emit(current_turn)
+	current_turn = 0 if current_turn else 1
+	turn_started.emit(current_turn)
